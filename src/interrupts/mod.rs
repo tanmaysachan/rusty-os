f1466132b2e7b439c3b30ecbb84bd01cd85bf4b4
Tanmay Sachan <tnmysachan@gmail.com>
2020-06-22 11:23:09 +0530

    Add static idt

--- src/interrupts/mod.rs
+++ src/interrupts/mod.rs
@@ -1,1 +1,9 @@
 mod interrupt_descriptor_table;
+use lazy_static::lazy_static;
+
+lazy_static! {
+    static ref IDT: interrupt_descriptor_table::Idt = {
+        let mut idt = interrupt_descriptor_table::Idt::new();
+        idt
+    };
+}
